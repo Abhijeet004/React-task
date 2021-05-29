b8a9e0e8b2c605c5b80e597d4bb551ba26b639aa
Abhijeet004 <17ucc004@lnmiit.ac.in>
2021-05-29 12:48:12 +0530

    initial setup

---
+++ src/components/SearchBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field ">
+              <label>Image Search</label>
+            <input
+              type="text"
+              placeholder="Search any image "
+            />
+           
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
